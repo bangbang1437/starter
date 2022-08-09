4aa92943e64344b9743923c2325f055e2f8b190a
Your Name <you@example.com>
2022-08-09 23:04:25 +0300

    slider Added

--- src/js/app.js
+++ src/js/app.js
@@ -3,28 +3,54 @@ import $ from 'jquery';
 import * as WOW from "es-module-lexer";
 
 $(document).ready(function () {
-    //JQuery code here
+    var partnersImg = document.querySelectorAll(".rev");
+    console.log(partnersImg);
+    partnersImg.forEach((item, index) => {
+
+        item.addEventListener("click", function (e) {
+            partnersImg.forEach(item => {
+                item.classList.remove("active")
+            })
+            item.classList.toggle("active")
+            comments_Changer(index)
+        })
+    })
 });
 
+const comments_Changer = (index) => {
+    var comments = document.querySelectorAll(".item_rev");
+    comments.forEach(item => {
+        item.classList.remove("active")
+    })
+    var comments_item = comments[index]
+    comments_item.classList.toggle("active")
+
+}
+
+
 /* Function can help use fixed header*/
-    function changeBackground(curOffsetTop) {
-        const header = document.querySelector("#header")
-        const className = 'is_scrolled'
-        if (curOffsetTop > 100)
-            return header.classList.add(className)
+function changeBackground(curOffsetTop) {
+    const header = document.querySelector("#header")
+    const className = 'is_scrolled'
+    if (curOffsetTop > 100)
+        return header.classList.add(className)
 
-        if (curOffsetTop < 100)
-            return header.classList.remove(className)
-    }
+    if (curOffsetTop < 100)
+        return header.classList.remove(className)
+}
 
-    document.addEventListener('scroll', function (e) {
-        const curOffsetTop = window.pageYOffset
-        changeBackground(curOffsetTop)
-    });
+document.addEventListener('scroll', function (e) {
+    const curOffsetTop = window.pageYOffset
+    changeBackground(curOffsetTop)
+});
 
-    /* Fixation all img . User cant move them */
+/* Fixation all img . User cant move them */
 
-    $("img, a").on("dragstart", function (event) {
-        event.preventDefault();
-    });
+$("img, a").on("dragstart", function (event) {
+    event.preventDefault();
+});
+
+
+
+
 
